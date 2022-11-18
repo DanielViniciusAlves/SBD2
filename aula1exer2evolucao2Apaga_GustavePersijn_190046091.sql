d7dbd2b2f741ef4163e786eb73e050fe7418bcef
Daniel Alves <danielribeiroalves0@gmail.com>
2022-11-18 08:41:55 -0300

    Adding exercises.

---
+++ aula1exer2evolucao2Apaga_GustavePersijn_190046091.sql
@@ -0,0 +1,26 @@
+-- ---------------------   << Empresa de Vendas >>   ---------------------
+--
+--                    SCRIPT DE APAGAR (DDL)
+-- 
+-- Data Criacao ...........: 08/11/2022
+-- Autor(es) ..............: Gustave A. Persijn
+-- Banco de Dados .........: MySQL 8.0
+-- Banco de Dados(nome) ...: aula1exer2
+-- 
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 09 Tabelas
+-- 
+-- -------------------------------------------------------------------------
+
+USE aula1exer2ev2;
+
+DROP TABLE IF EXISTS contem;
+DROP TABLE IF EXISTS VENDA;
+DROP TABLE IF EXISTS telefone;
+DROP TABLE IF EXISTS PRODUTO;
+DROP TABLE IF EXISTS SUPERVISAO_monitora;
+DROP TABLE IF EXISTS AREA;
+DROP TABLE IF EXISTS EMPREGADO;
+DROP TABLE IF EXISTS GERENTE;
+DROP TABLE IF EXISTS FUNCIONARIO;
